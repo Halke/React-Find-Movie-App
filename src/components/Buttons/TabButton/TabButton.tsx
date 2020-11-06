a656cf863fda3e0b829d9588c66ea213719eb5b2
Halke <dinohalilovichalke@gmail.com>
2020-11-06 10:57:30 +0100

    Added button for selecting the tab

---
+++ src/components/Buttons/TabButton/TabButton.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function TabButton({btnText, isActive}) {
+
+    const [active, setActive] = useState(isActive)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TabButton
