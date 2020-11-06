8304d177031c8c4db4b46db11a720d7962b5f282
Halke <dinohalilovichalke@gmail.com>
2020-11-06 12:00:00 +0100

    Added comments for every task that should be done

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import "./styles.css";
+import React, {useState} from 'react'
+import TabButton from '../Buttons/TabButton/TabButton'
+import './styles.css'
 
 function Tab() {
+
+    const [moviesActive, setMoviesActive] = useState(true)
+
     return (
-        <div>
-            
+        <div className="tab">
+            {/* Buttons za mijenjanje tabova */}
+            <div className="tab__change-tab-btns">
+                {/* Movies TAB */}
+                <TabButton 
+                    onClick={() => setMoviesActive(!moviesActive)} 
+                    buttonText="Movies" 
+                    isActive={moviesActive ? true : false} 
+                />
+
+                {/* TV Shows TAB */}
+                <TabButton 
+                    onClick={() => setMoviesActive(!moviesActive)} 
+                    buttonText="TV Shows" 
+                    isActive={moviesActive ? false : true} 
+                />
+            </div>
+
+            {/* Search Bar Section */}
+            <div className="tab__search-bar">
+
+            </div>
+
+            {/* Tab za prikaz sadrzaja */}
+            <div className="tab__content">
+
+            </div>
         </div>
     )
 }
