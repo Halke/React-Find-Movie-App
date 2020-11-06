a536d848ed6be7912b65548d2abedd4faa669345
Halke <dinohalilovichalke@gmail.com>
2020-11-07 00:34:15 +0100

    Added store and provider to our app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, Store } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App'
+import reducer from './components/store/reducer'
+
+const store: Store<SearchState, SearchAction> & {
+  dispatch: DispatchType
+} = createStore(reducer)  
+
+const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+  ,
+  rootElement
 )
