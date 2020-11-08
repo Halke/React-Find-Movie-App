9bf04e84ed991c44c0cba54c0f00648ed2850dbc
Halke <dinohalilovichalke@gmail.com>
2020-11-08 12:29:44 +0100

    Added the regular content

--- src/components/Tab/TabContent/TabContent.tsx
+++ src/components/Tab/TabContent/TabContent.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import SearchContent from '../../Content/SearchContent/SearchContent'
+import RegularContent from '../../Content/RegularContent/RegularContent'
+import {SearchContext} from '../../context/SearchContext'
 import './styles.css'
 
 function TabContent() {
+
+    const {searchOnFocus, contentType} = useContext(SearchContext)
+
+    function renderSearchContent(){
+        return <SearchContent />
+    }
+
+    function renderRegularContent(){
+        return (
+            <RegularContent />
+        )
+    }
+
     return (
         <div className='tab-content'>
-            
+            {searchOnFocus ? renderSearchContent : renderRegularContent}
         </div>
     )
 }
