33f9cce0c1dc3f92f9f7e2d93b647f6dd1a47c82
Halke <dinohalilovichalke@gmail.com>
2020-11-08 10:37:46 +0100

    Component for displaying content on tabs

---
+++ src/components/Tab/TabContent/TabContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+function TabContent() {
+    return (
+        <div className='tab-content'>
+            
+        </div>
+    )
+}
+
+export default TabContent
