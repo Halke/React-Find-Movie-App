428dbbe13ca108128d8b7df13a09a801734325ba
Halke <dinohalilovichalke@gmail.com>
2020-11-09 10:14:29 +0100

    Added axios get method

--- src/components/SingleContent/SingleContent.tsx
+++ src/components/SingleContent/SingleContent.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+import {SearchContext} from '../context/SearchContext'
+import axios from 'axios'
 import './styles.css'
 
-function SingleContent() {
+function SingleContent(props: any) {
+
+    const [content, setContent] = useState({})
+
+    const { id } = props.match.params
+    const { contentType } = useContext(SearchContext)
+
+    useEffect(() => {
+        
+    }, [id])
+
     return (
         <div>
             
