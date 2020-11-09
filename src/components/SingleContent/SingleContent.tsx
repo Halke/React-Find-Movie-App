e7c3d1703d31f7bacc3f49e67ba1bf3e071d6644
Halke <dinohalilovichalke@gmail.com>
2020-11-09 14:50:36 +0100

    Added back button

--- src/components/SingleContent/SingleContent.tsx
+++ src/components/SingleContent/SingleContent.tsx
@@ -4,46 +4,70 @@ import {SearchContext} from '../context/SearchContext'
 import axios from 'axios'
 import './styles.css'
 
+
 function SingleContent(props: any) {
 
-    const [content, setContent] = useState({
-        title: '',
-        name: '',
+    const [content, setContent] = useState<any>({
+        backdrop_path: '',
         poster_path: '',
-        overview: ''
+        overview: '',
+        videos: {
+            results: [
+                {
+                    key: ''
+                }
+            ]
+        }
     })
-    const [videoKey, setVideoKey] = useState('')
 
     const { id } = props.match.params
     const { contentType } = useContext(SearchContext)
 
-    //  https://api.themoviedb.org/3/movie/121?api_key=d12848de02e2a36cbdfe60e9860f6f6c&language=en-US&append_to_response=videos
-    //  https://api.themoviedb.org/3/tv/2140?api_key=d12848de02e2a36cbdfe60e9860f6f6c&language=en-US&append_to_response=videos
-
     useEffect(() => {
         axios.get('https://api.themoviedb.org/3/' + contentType + '/' + id + '?api_key=d12848de02e2a36cbdfe60e9860f6f6c&language=en-US&append_to_response=videos')
         .then(res => {
-            setContent(prevVal => ({...prevVal, ...res.data}))
-            setVideoKey(res.data.video.results[0].key)
+            setContent(res.data)
         })
         .catch(err => console.log('ERROR: ', err))
     }, [id])
 
+
     return (
         <div className='single-content'>
+            <button className='single-content__back-btn'>
+                <span className='single-content__back-btn-icon'>
+                    <img src={process.env.PUBLIC_URL + '/assets/icons/back-icon.png'} alt='Back icon'/>
+                </span>
+                BACK
+            </button>
             <div className='single-content__image-or-video'>
-                {videoKey !== '' ? 
+                {content.videos.results[0].key !== '' ? 
                     <iframe 
-                        width="600" 
-                        height="350" 
-                        src="https://www.youtube.com/embed/_1xxsS8NugI" 
+                        width="700" 
+                        height="450" 
+                        src={'https://www.youtube.com/embed/' + content.videos.results[0].key} 
                         frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                         allowFullScreen>
                     </iframe>
                 :
-                    <img src={'https://image.tmdb.org/t/p/w300' + content.poster_path} alt={contentType + ' poster'}/>
+                    <img 
+                        className='single-content__image' 
+                        src={content.backdrop_path !== '' ?
+                            'https://image.tmdb.org/t/p/w500' + content.backdrop_path
+                            :
+                            'https://image.tmdb.org/t/p/w400' + content.poster_path} 
+                        alt={contentType + ' poster'}
+                    />
                 }
+            </div>
+            <div>
+                <h1 className='single-content__title'>
+                    {content.title || content.name}
+                </h1>
+                <p className='single-content__overview'>
+                    
+                </p>
             </div>
         </div>
     )
