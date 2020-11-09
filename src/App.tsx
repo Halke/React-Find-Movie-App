77d1268e035878b1f35b551ae8b5dd42cf8a5182
Halke <dinohalilovichalke@gmail.com>
2020-11-09 14:50:50 +0100

    Added route for the single content

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Tab from './components/Tab/Tab'
 import {SearchContext, searchContextDefaultValue} from './components/context/SearchContext'
 import useSearchContextValue from './components/context/useSearchContextValue'
+import SingleContent from './components/SingleContent/SingleContent'
 
 import './App.css'
 
@@ -16,8 +17,8 @@ function App() {
         <div className="App">
             <Switch>
               <Route exact path="/" component={Tab}/>
-              <Route exact path="/movie/:movieId" />
-              <Route exact path="/tv/:tvId" />
+              <Route exact path="/movie/:id" component={SingleContent}/>
+              <Route exact path="/tv/:id" component={SingleContent}/>
             </Switch>
         </div>
       </Router>
